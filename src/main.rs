60d7c128b5441b695d3ada52b728a0c395313d6f
yanorei32 <yanorei@hotmail.co.jp>
2025-04-22 12:44:27 +0900

    Improve Configure WebHook Script

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,19 @@
+use std::collections::HashMap;
+use std::env;
+
 use clap::{Parser, Subcommand};
+use reqwest::{header::AUTHORIZATION, StatusCode};
+use serde::Deserialize;
 use serenity::{
     all::{ChannelType, GuildId},
     builder::{CreateChannel, CreateWebhook},
     prelude::*,
 };
-use std::env;
+
+#[derive(Debug, Deserialize)]
+struct Ratelimit {
+    retry_after: f32,
+}
 
 #[derive(Debug, Parser)]
 #[command(author, version, about, long_about = None)]
@@ -21,6 +30,11 @@ pub enum Commands {
     Webhook { guild_id: u64 },
 }
 
+#[derive(Debug, Deserialize)]
+struct WebHook {
+    url: String,
+}
+
 #[tokio::main]
 async fn main() {
     let cli = Cli::parse();
@@ -28,9 +42,14 @@ async fn main() {
     let token = env::var("DISCORD_TOKEN").expect("DISCORD_TOKEN");
     let intents = GatewayIntents::non_privileged();
 
-    let client = Client::builder(token, intents)
+    let client = Client::builder(&token, intents)
         .await
         .expect("Error creating client");
+
+    let webclient = reqwest::Client::builder()
+        .user_agent("DiscordWebHookConfigurator/0.1.0")
+        .build()
+        .unwrap();
 
     match cli.command {
         Commands::Webhook { guild_id } => {
@@ -39,16 +58,42 @@ async fn main() {
 
             for (cid, x) in channels {
                 let url = loop {
-                    let w = CreateWebhook::new("Explosion");
-                    match cid.create_webhook(&client.http, w).await {
-                        Ok(webhook) => break webhook.url(),
-                        Err(e) => {
-                            eprintln!("Create channel error {e:?}... wait 15s...");
-                        }
+                    let resp = webclient
+                        .post(format!(
+                            "https://discord.com/api/v10/channels/{cid}/webhooks"
+                        ))
+                        .header(AUTHORIZATION, format!("Bot {token}"))
+                        .json(&{
+                            let mut req = HashMap::new();
+                            req.insert("name".to_string(), "explosion".to_string());
+                            req
+                        })
+                        .send()
+                        .await
+                        .unwrap();
+
+                    let status = resp.status();
+
+                    if status.is_success() {
+                        let webhook: WebHook = resp.json().await.unwrap();
+                        break webhook.url;
+                    }
+
+                    if status == StatusCode::TOO_MANY_REQUESTS {
+                        let ratelimit: Ratelimit = resp.json().await.unwrap();
+                        eprintln!("Ratelimit exceeded! sleep {}s", ratelimit.retry_after);
+                        tokio::time::sleep(std::time::Duration::from_secs_f32(ratelimit.retry_after)).await;
+                        continue;
+                    }
+
+                    if status.is_client_error() {
+                        panic!("UNKNOWN {status:?}");
+                    } else {
+                        continue;
                     }
                 };
 
-                println!("{} {}", x.name, url.unwrap());
+                println!("{} {}", x.name, url);
             }
         }
         Commands::Explosion { guild_id } => {
