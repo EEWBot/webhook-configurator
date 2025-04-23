d0ac4dff58583cb7e1ef7aec671b1793f60837f1
yanorei32 <yanorei@hotmail.co.jp>
2025-04-23 09:00:04 +0900

    Add export function and improve retry strategy

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use reqwest::{header::AUTHORIZATION, StatusCode};
 use serde::Deserialize;
 use serenity::{
     all::{ChannelType, GuildId},
-    builder::{CreateChannel, CreateWebhook},
+    builder::CreateChannel,
     prelude::*,
 };
 
@@ -26,6 +26,8 @@ pub struct Cli {
 pub enum Commands {
     #[command(about = "Create explosion")]
     Explosion { guild_id: u64 },
+    #[command(about = "Export Webhook")]
+    Export { guild_id: u64 },
     #[command(about = "Create Webhook")]
     Webhook { guild_id: u64 },
 }
@@ -52,11 +54,108 @@ async fn main() {
         .unwrap();
 
     match cli.command {
+        Commands::Export { guild_id } => {
+            let guild = GuildId::new(guild_id);
+            let channels = guild.channels(&client.http).await.unwrap();
+
+            let mut channels: Vec<_> = channels.iter().collect();
+
+            channels.sort_by_cached_key(|(_id, chan)| {
+                let (_, n) = chan.name.split_once('-').unwrap();
+                n.parse::<u32>().unwrap()
+            });
+
+            for (cid, x) in channels {
+                let url = loop {
+                    let resp = webclient
+                        .get(format!(
+                            "https://discord.com/api/v10/channels/{cid}/webhooks"
+                        ))
+                        .header(AUTHORIZATION, format!("Bot {token}"))
+                        .send()
+                        .await
+                        .unwrap();
+
+                    let status = resp.status();
+
+                    if status.is_success() {
+                        let webhook: Vec<WebHook> = resp.json().await.unwrap();
+
+                        break webhook.iter().next().unwrap().url.clone();
+                    }
+
+                    if status == StatusCode::TOO_MANY_REQUESTS {
+                        let ratelimit: Ratelimit = resp.json().await.unwrap();
+                        eprintln!("Ratelimit exceeded! sleep {}s", ratelimit.retry_after);
+                        tokio::time::sleep(std::time::Duration::from_secs_f32(
+                            ratelimit.retry_after,
+                        ))
+                        .await;
+                        continue;
+                    }
+
+                    if status.is_client_error() {
+                        panic!("UNKNOWN {status:?}");
+                    } else {
+                        continue;
+                    }
+                };
+
+                println!("{} {}", x.name, url);
+            }
+        }
         Commands::Webhook { guild_id } => {
             let guild = GuildId::new(guild_id);
             let channels = guild.channels(&client.http).await.unwrap();
 
+            let mut channels: Vec<_> = channels.iter().collect();
+
+            channels.sort_by_cached_key(|(_id, chan)| {
+                let (_, n) = chan.name.split_once('-').unwrap();
+                n.parse::<u32>().unwrap()
+            });
+
             for (cid, x) in channels {
+                let already_exists = loop {
+                    let resp = webclient
+                        .get(format!(
+                            "https://discord.com/api/v10/channels/{cid}/webhooks"
+                        ))
+                        .header(AUTHORIZATION, format!("Bot {token}"))
+                        .send()
+                        .await
+                        .unwrap();
+
+                    let status = resp.status();
+
+                    if status.is_success() {
+                        let webhook: Vec<WebHook> = resp.json().await.unwrap();
+
+                        break webhook.iter().next().is_some();
+                    }
+
+                    if status == StatusCode::TOO_MANY_REQUESTS {
+                        let ratelimit: Ratelimit = resp.json().await.unwrap();
+                        eprintln!("Ratelimit exceeded! sleep {}s", ratelimit.retry_after);
+                        tokio::time::sleep(std::time::Duration::from_secs_f32(
+                            ratelimit.retry_after,
+                        ))
+                        .await;
+                        continue;
+                    }
+
+                    if status.is_client_error() {
+                        panic!("UNKNOWN {status:?}");
+                    } else {
+                        continue;
+                    }
+                };
+
+                if already_exists {
+                    eprintln!("Already Exists, for {} cancel.", x.name);
+                    continue;
+                }
+
                 let url = loop {
                     let resp = webclient
                         .post(format!(
@@ -82,7 +181,10 @@ async fn main() {
                     if status == StatusCode::TOO_MANY_REQUESTS {
                         let ratelimit: Ratelimit = resp.json().await.unwrap();
                         eprintln!("Ratelimit exceeded! sleep {}s", ratelimit.retry_after);
-                        tokio::time::sleep(std::time::Duration::from_secs_f32(ratelimit.retry_after)).await;
+                        tokio::time::sleep(std::time::Duration::from_secs_f32(
+                            ratelimit.retry_after,
+                        ))
+                        .await;
                         continue;
                     }
 
